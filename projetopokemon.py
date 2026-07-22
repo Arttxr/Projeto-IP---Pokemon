40d5ffbc320bb4c54556841acd4c91edcf316111
Arttxr <arturaraujo853211@gmail.com>
2026-07-22 16:05:01 -0300

    Update projetopokemon.py
    versão 1.2

--- projetopokemon.py
+++ projetopokemon.py
@@ -12,18 +12,30 @@ with open("pokedex.txt", "r", encoding="utf-8") as arquivo:
     pokedex = arquivo.read().split(',')
 
 #funçoes da pokedex!
-def registrarPokemon(numeros, pokemons, tipos):
+def registrarPokemon(pokemons, tipos):
     numeroTirado = random.randint(1, 152)
     indice = numeroTirado - 1
     print (f'Parabéns, você capturou um(a) {pokemons[indice]}')
     print (f'Número do Pokemon na Pokedex: #{indice +1}')
     print (f'Tipo(s)do Pokemon: {tipos[indice]}')
     if pokemons[indice].lower() not in pokedex[indice].lower():
-        pokedex[indice] = f'#{indice + 1}: {pokemons[indice]}'
+        pokedex[indice] = f'#{indice + 1}: {pokemons[indice]} tipo: {tipos[indice]}'
         print("Pokémon registrado na Pokédex!")
     else:
         print("Você já havia capturado esse Pokémon!")
     with open("pokedex.txt", "w", encoding="utf-8") as arquivo:
         arquivo.write(",".join(pokedex))
-    
-registrarPokemon(numeros, pokemons, tipos) 
+
+def pesquisarNaPokedex(pokedex):
+    pokemonApesquisar = input('Qual nome ou número do Pokemon que você quer encontrar na pokedex? (Ex: #2 // Ivysaur)').strip().lower()
+    for pokemon in pokedex:
+        dados = pokemon.split()
+        numero = dados[0].replace(":", "")
+        nome = dados[1].lower()
+
+        if pokemonApesquisar == numero or pokemonApesquisar == nome:
+            print (pokemon)
+            
+        
+    print ("Pokémon não registrado ou nome digitado errado.")
+
